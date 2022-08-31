749bf390cc79a21fef3a6abbf9118b23ea554284
Arnaud <arnaud.chanoine@gmail.com>
2022-08-31 21:55:46 +0200

    Blackify code and fix PEP8 warnings

--- cam.py
+++ cam.py
@@ -1,50 +1,53 @@
 from cv2 import cv2
 import numpy as np
 
+
 def main():
-  vc = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(0)
 
-  if vc.isOpened():
-    rval, frame = vc.read()
-  else:
-    rval = False
+    if video_capture.isOpened():
+        rval, frame = video_capture.read()
+    else:
+        rval = False
 
-  while rval:
-    rval, frame = vc.read()
-    print(toASCII(frame))
+    while rval:
+        rval, frame = video_capture.read()
+        print(to_ascii(frame))
 
-    key = cv2.waitKey(50) # 50ms pause -> ~20fps
-    # Press echap to end
-    if key == 27:
-      break
+        # Refresh every 50ms -> ~20fps
+        key = cv2.waitKey(50)
 
-def toASCII(frame, cols = 120, rows = 35):
+        # Press escape to end
+        if key == 27:
+            break
 
-  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  height, width = frame.shape
-  cell_width = width / cols
-  cell_height = height / rows
-  if cols > width or rows > height:
-    raise ValueError('Too many cols or rows.')
-  result = ""
-  for i in range(rows):
-    for j in range(cols):
-      gray = np.mean(
-        frame[int(i * cell_height):min(int((i + 1) * cell_height), height), int(j * cell_width):min(int((j + 1) * cell_width), width)]
-      )
-      result += grayToChar(gray)
-    result += '\n'
-  return result
 
-def grayToChar(gray):
-  CHAR_LIST = ' .:-=+*#%@'
-  num_chars = len(CHAR_LIST)
-  return CHAR_LIST[
-    min(
-      int(gray * num_chars / 255),
-      num_chars - 1
-    )
-  ]
-  
-if __name__ == '__main__':
+def to_ascii(frame, cols=240, rows=70):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    height, width = frame.shape
+    cell_width = width / cols
+    cell_height = height / rows
+    if cols > width or rows > height:
+        raise ValueError("Too many cols or rows.")
+    result = ""
+    for i in range(rows):
+        for j in range(cols):
+            gray = np.mean(
+                frame[
+                    int(i * cell_height) : min(int((i + 1) * cell_height), height),
+                    int(j * cell_width) : min(int((j + 1) * cell_width), width),
+                ]
+            )
+            result += gray_to_char(gray)
+        result += "\n"
+    return result
+
+
+def gray_to_char(gray):
+    char_list = " .:-=+#%@"
+    num_chars = len(char_list)
+    return char_list[min(int(gray * num_chars / 255), num_chars - 1)]
+
+
+if __name__ == "__main__":
     main()
